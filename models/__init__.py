09281642cd378c394fee373ae618684c82e6d7dd
Kaare <kaare@clearsky.vision>
2024-12-03 19:23:32 +0100

    remove unused model

--- models/__init__.py
+++ models/__init__.py
@@ -4,4 +4,3 @@ Collection of models used by the ClearSKY Vision API
 
 from models.request_response_models import *
 from models.enums import *
-from models.GeoJsonGeometryDto import GeoJsonGeometryDto
